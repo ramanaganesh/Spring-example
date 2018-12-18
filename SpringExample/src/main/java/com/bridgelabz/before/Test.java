2dceb06381fe3f7831ccf8ba6e02477bd269de10
ramanaganesh <ramanaganesh08@gmail.com>
2018-12-18 19:20:23 +0530

    spring

---
+++ SpringExample/src/main/java/com/bridgelabz/before/Test.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.before;
+
+
+import org.springframework.context.ApplicationContext;  
+import org.springframework.context.support.ClassPathXmlApplicationContext;  
+public class Test{  
+  public static void main(String[] args){  
+      ApplicationContext context = new ClassPathXmlApplicationContext("AppllicationContext.xml");  
+      Operation e = (Operation) context.getBean("opBean");  
+      System.out.println("calling msg...");  
+      e.msg();  
+      System.out.println("calling m...");  
+      e.m();  
+      System.out.println("calling k...");  
+      e.k();  
+  }  
+}  
--- SpringExample/src/main/java/com/bridgelabz/setterdependencyinjection/Exam.java
+++ SpringExample/src/main/java/com/bridgelabz/setterdependencyinjection/Exam.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.setterdependencyinjection;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Exam 
@@ -8,10 +9,12 @@ public class Exam
 	public static void main(String[] args) {
 		
 	
+
 	ApplicationContext context=new ClassPathXmlApplicationContext("bean.xml");
 	Student student=(Student) context.getBean("student");
 	student.display();
 	Student student1=(Student) context.getBean("student1");
 	student1.display();
+	((ConfigurableApplicationContext)context).close();
 	}
 }
